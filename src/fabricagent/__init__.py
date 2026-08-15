fc24adc4855e93d6e843d7e47b00c4980ef59c0a
L0g1kL1f3 <113134732+L0g1kL1f3@users.noreply.github.com>
2026-08-15 13:15:09 +0200

    Update __init__.py

--- src/fabricagent/__init__.py
+++ src/fabricagent/__init__.py
@@ -1,6 +1,7 @@
 """Public API for the fabricagent package."""
 
 from .VerifyAgent import find_empty_fields
+from .MarkDownPDF import move_file , transform_markdown
 
-__all__ = ["find_empty_fields"]
+__all__ = ["find_empty_fields","move_file","transform_markdown"]
 __version__ = "0.1.0"

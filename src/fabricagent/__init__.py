422f710b065ae1ed9e35db41f80e9c35e9eab18c
L0g1kL1f3 <113134732+L0g1kL1f3@users.noreply.github.com>
2026-08-15 13:30:37 +0200

    Update __init__.py

--- src/fabricagent/__init__.py
+++ src/fabricagent/__init__.py
@@ -4,4 +4,4 @@ from .VerifyAgent import find_empty_fields
 from .MarkDownPDF import move_file , transform_markdown
 
 __all__ = ["find_empty_fields","move_file","transform_markdown"]
-__version__ = "0.1.0"
+__version__ = "0.1.1"

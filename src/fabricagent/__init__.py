2a6a38caeb62032f87ef2e7caff1bef5b6b6bd8c
L0g1kL1f3 <113134732+L0g1kL1f3@users.noreply.github.com>
2026-08-16 16:16:29 +0200

    update url list

--- src/fabricagent/__init__.py
+++ src/fabricagent/__init__.py
@@ -2,11 +2,13 @@
 
 from .markdown_pdf import move_file, transform_markdown
 from .verify_agent import find_empty_fields
+from .list_paths import get_pdfs
 
 __all__ = [
     "find_empty_fields",
     "transform_markdown",
     "move_file",
+    get_pdfs
 ]
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
---
+++ src/fabricagent/list_paths.py
@@ -0,0 +1,24 @@
+import requests
+
+
+def get_pdfs(Url,path):
+    output = []
+    fullPath = f"{Url}{path}"
+
+    response = requests.get(fullPath)
+    response.raise_for_status()
+
+    items = response.json()
+
+    for item in items:
+        if item["type"] == "dir":
+            get_pdfs(item["path"])
+
+        elif item["type"] == "file" and item["name"].lower().endswith(".pdf"):
+            output.append({
+                "DocumentUrl": item["download_url"],
+                "SourceFolder": f"Files/{path.split('/')[-1]}",
+                "FileName": item["name"]
+            })
+
+    return output

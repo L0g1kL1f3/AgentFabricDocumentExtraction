57dcfad9745ab1342efb9edaa04052f9019caba4
L0g1kL1f3 <113134732+L0g1kL1f3@users.noreply.github.com>
2026-08-16 19:56:02 +0200

    list file fix

--- src/fabricagent/__init__.py
+++ src/fabricagent/__init__.py
@@ -2,13 +2,14 @@
 
 from .markdown_pdf import move_file, transform_markdown
 from .verify_agent import find_empty_fields
-from .list_paths import get_pdfs
+from .list_paths import get_pdfs, get_item_list
 
 __all__ = [
     "find_empty_fields",
     "transform_markdown",
     "move_file",
-    get_pdfs
+    get_pdfs,
+    get_item_list
 ]
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
